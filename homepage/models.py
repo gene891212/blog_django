fd55e5ac3f35f1bc1526e7cc4e237e19a986d4e9
Tsung-Justin <king90131@gmail.com>
2020-11-25 16:25:08 +0800

    upload Post in homepage.models.py

--- homepage/models.py
+++ homepage/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 class Post(models.Model):
     username = models.CharField("使用者", max_length=50)
-    title = models.TextField(max_length=100)
-    # 123
+    title = models.TextField('標題', max_length=100)
+    subtitle = models.TextField('副標', max_length=100)
+    content = models.TextField('內文', max_length=200)
+    date = models.DateField(auto_now_add=True)

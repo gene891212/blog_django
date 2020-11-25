ce963ed30363bc5ad93530632f9894dda2f667f2
gene_hsieh <hc360000@gene-hsieh-deMacBook-Air.local>
2020-11-25 17:41:46 +0800

    upload homepage posthtml feature

--- homepage/models.py
+++ homepage/models.py
@@ -4,9 +4,9 @@ class Post(models.Model):
     username = models.CharField("使用者", max_length=50)
     title = models.TextField('標題', max_length=100)
     subtitle = models.TextField('副標', max_length=100, blank=True)
-    content = models.TextField('內文', max_length=200)
+    content = models.TextField('內文')
     date = models.DateField(auto_now_add=True)
-
+    datetime = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title
     
--- homepage/views.py
+++ homepage/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
-# from .model import *
-# Create your views here.
-# def homepage(request):
-#     return render(request, 'index.html', context={pic: ''})
+from .models import *
+from datetime import datetime
 
+# Create your views here.
 
 class Homepage(generic.ListView):
-    # model =
+    model = Post
+    now = model.datetime.timestamp
+    hi = datetime.fromtimestamp(now)
+
     def homepage(request):
-        return render(request, 'index.html', context={pic: ''})
+        return render(request, 'index.html', context={pic: self.hi})

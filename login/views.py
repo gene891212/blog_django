b4e46769d34b93aa3e3abff4d86264d6194ec7c9
Tsung-Justin <king90131@gmail.com>
2020-11-26 14:58:25 +0800

    upload login, logout, register feature.

--- login/views.py
+++ login/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from .forms import *
 
-class Login():
-    pass
+def sign_up(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/login')
+        else:
+            return render(request, 'login/register.html', {'form':form})
+    else:
+        form = RegisterForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'login/register.html', context)
 
-class Logout():
-    pass
+def sign_in(request):
+    form = LoginForm()
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+
+    context = {
+        'form' : form
+    }
+    return render(request, 'login/login.html', context)
+
+def log_out(request):
+    logout(request)
+    return redirect('/login')
